fcfddb24d3abe3f04b3a9da1fb7d5f884635ad15
hyoin <144425658+hyoinYang@users.noreply.github.com>
2024-08-20 11:56:56 +0900

    [Gold V] Title: 전역 역전, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/31565. 전역 역전/전역 역전.cc
+++ 백준/Gold/31565. 전역 역전/전역 역전.cc
@@ -10,7 +10,6 @@ using namespace std;
 int t, n;
 int yeong[3];
 int jong[3];
-vector<tuple<int, int, int>> comm;
 int dp[10010];
 
 int day[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -20,40 +19,23 @@ bool is_leap(int y) {
 }
 
 int days(int y, int m, int d) {
-	int ret = 0;
+	int result = 0;
 	for (int i = 0; i < y; i++) {
-		ret += is_leap(i) ? 366 : 365;
+		if (is_leap(i))
+			result += 366;
+		else
+			result += 365;
 	}
 	bool leap = is_leap(y);
 	for (int i = 1; i < m; i++) {
-		ret += (leap && i == 2) ? day[i - 1] + 1 : day[i - 1];
+		if (leap && i == 2)
+			result += day[i - 1] + 1;
+		else
+			result += day[i - 1];
 	}
-	ret += d;
-	return ret;
+	result += d;
+	return result;
 }
-/*
-int knapsack(int idx, int t) {
-	if (t < 0) return INT_MIN;;
-	if (idx == n) {
-		return days(yeong[0], yeong[1], yeong[2]) - days(jong[0], jong[1], yeong[2]);
-	}
-	int do_something, do_nothing;
-	switch (get<0>(comm[idx])) {
-	case 1:
-		do_something = knapsack(idx + 1, t - get<1>(comm[idx])) + get<2>(comm[idx]);
-		break;
-	case 2:
-		do_something = knapsack(idx + 1, t - get<1>(comm[idx])) + get<2>(comm[idx]);
-		break;
-	case 3:
-		do_something = knapsack(idx + 1, t - get<1>(comm[idx])) + 30 * get<2>(comm[idx]);
-		break;
-	}
-	do_nothing = knapsack(idx + 1, t);
-	return max(do_nothing, do_something);
-	
-}
-*/
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -67,11 +49,10 @@ int main() {
 	}
 
 	cin >> t >> n;
-	
-	//comm.resize(n);
 
 	int diff = days(jong[0], jong[1], jong[2]) - days(yeong[0], yeong[1], yeong[2]);
-	for (; n--;) {
+
+	while(n--) {
 		int op, c, k;
 		cin >> op >> c >> k;
 		if (op == 3) k *= 30;
@@ -79,6 +60,7 @@ int main() {
 			dp[i] = max(dp[i], dp[i - c] + k);
 		}
 	}
-	printf("%d", abs(diff - *max_element(dp, dp + t + 1)));
+
+	cout << abs(diff - *max_element(dp, dp + t + 1));
     return 0;
 }
