45d4da2046434b78e2673b7912de435f42db6fc8
양효인 <144425658+hyoinYang@users.noreply.github.com>
2025-10-19 16:53:29 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 3684 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -10,9 +10,8 @@ int main(){
 
     int N;
     vector<int> distance;
-    vector<int> city;
+    vector<int> oil_price;
     int result = 0;
-    int need_oil = 0;
 
     cin >> N;
 
@@ -20,31 +19,21 @@ int main(){
         int input;
         cin >> input;
         distance.push_back(input);
-
-        need_oil += input;
-    }
-
-    int min_oil_price = distance[0];
-
-    for (int i = 1; i < distance.size() - 1; i++){
-        min_oil_price = min_oil_price < distance[i] ? min_oil_price : distance[i];
     }
 
     for (int i = 0; i < N; i++){
         int input;
         cin >> input;
-        city.push_back(input);
+        oil_price.push_back(input);
     }
 
-    for (int i = 0; i < city.size(); i++){
-        if (city[i] == min_oil_price){
-            result += min_oil_price * need_oil;
-            break;
+    int min_oil_price = oil_price[0];
+
+    for (int i = 0; i < N - 1; i++){
+        if (oil_price[i] < min_oil_price){
+            min_oil_price = oil_price[i];
         }
-        else{
-            result += distance[i] * city[i];
-            need_oil -= distance[i];
-        }
+        result += min_oil_price * distance[i];
     }
 
     cout << result << endl;
