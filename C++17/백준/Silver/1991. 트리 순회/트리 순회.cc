b7a31588af75c81eca737a45905cbb11bb3d4220
양효인 <144425658+hyoinYang@users.noreply.github.com>
2025-11-11 17:51:20 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+map<char, pair<char, char>> tree;
+
+void preorder(char node)
+{
+    if (node == '.') return;
+    
+    cout << node;
+    preorder(tree[node].first);
+    preorder(tree[node].second);
+}
+
+void inorder(char node)
+{
+    if (node == '.') return;
+
+    inorder(tree[node].first);
+    cout << node;
+    inorder(tree[node].second);
+}
+
+void postorder(char node)
+{
+    if (node == '.') return;
+
+    postorder(tree[node].first);
+    postorder(tree[node].second);
+    cout << node;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        char root, left, right;
+        cin >> root >> left >> right;
+        tree[root] = {left, right};
+    }
+
+    preorder('A');
+    cout << '\n';
+
+    inorder('A');
+    cout << '\n';
+    
+    postorder('A');
+
+    return 0;
+
+}
